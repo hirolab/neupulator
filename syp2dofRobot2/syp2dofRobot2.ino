42e29a2eb55a9e7a6b6f8eef3f41027789e20eeb
Guilherme Aramizo Ribeiro <garamizo@mtu.edu>
2016-03-07 14:46:20 -0500

    Update syp2dofRobot2.ino

--- syp2dofRobot2/syp2dofRobot2.ino
+++ syp2dofRobot2/syp2dofRobot2.ino
@@ -4,17 +4,16 @@ Servo elbow, wrist;
 float min0 = 1024, max0 = 0, min1 = 1024, max1 = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+
   Serial.begin( 9600 );
   
-  elbow.attach( 2 );
-  wrist.attach( 3 );
+  elbow.attach( 4 );
+  wrist.attach( 5 );
   
+  // Calibration
   elbow.write( 90 );
   wrist.write( 90 );
-  
-  //while(1);
-  
+
   while( millis() < 5000 ) {
     // Filtering
     float average0 = 0, average1 = 0;
@@ -24,7 +23,6 @@ void setup() {
       average1 += analogRead( A1 ) / 10.0;
     }
     
-    // Online self tuning
     min0 = min( min0, average0 );
     max0 = max( max0, average0 );
     min1 = min( min1, average1 );
@@ -32,16 +30,9 @@ void setup() {
     
     delay( 50 );
   }
-  
-  Serial.print("Calibration data: ");
-  Serial.print( min0 ); Serial.print('\t');
-  Serial.print( max0 ); Serial.print('\t');
-  Serial.print( min1 ); Serial.print('\t');
-  Serial.print( max1 ); Serial.print('\n');
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   
   // Filtering
   float average0 = 0, average1 = 0;
@@ -51,36 +42,13 @@ void loop() {
     average1 += analogRead( A1 ) / 10.0;
   }
   
-//  // Online self tuning
-//  min0 = min( min0, average0 );
-//  max0 = max( max0, average0 );
-//  min1 = min( min1, average1 );
-//  max1 = max( max1, average1 );
-  
-  // Joint angle estimation
-  int angElbow = map( average0, min0, max0, 0, 180 );
-  int angWrist = map( average1, min1, max1, 30, 180 );
-  
-  // Motion constrain
-  //angElbow = constrain( angElbow, 10, 180 );
-  //angWrist = constrain( angWrist, max( 0, angElbow + 10), 180 );
+  // Joint angle mapping
+  int angElbow = map( average0, min0, max0, 30, 150 );
+  int angWrist = map( average1, min1, max1, 150, 30 );
   
   // Actuate
   elbow.write( angElbow );
   wrist.write( angWrist );
-//  elbow.write( 90 );
-//  wrist.write( 90 );
-  
   
   delay(50);
-  
-  // Print for callibration
-  Serial.print( analogRead(A0) );
-  Serial.print('\t');
-  Serial.println( analogRead(A1) );
-
-  // Print angles
-//  Serial.print( angElbow );
-//  Serial.print('\t');
-//  Serial.println( angWrist );
 }
