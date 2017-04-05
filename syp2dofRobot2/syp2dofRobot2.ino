75a8fa56c5804d45c3a5313b99f5d688dafb8097
Guilherme Aramizo Ribeiro <garamizo@mtu.edu>
2017-04-05 18:54:20 -0400

    Calibrates on the run

--- syp2dofRobot2/syp2dofRobot2.ino
+++ syp2dofRobot2/syp2dofRobot2.ino
@@ -7,36 +7,29 @@ void setup() {
 
   Serial.begin( 9600 );
   
-  elbow.attach( 9 );
-  wrist.attach( 8 );
-  
-  // Calibration
-  elbow.write( 0 );
-  wrist.write( 90 );
-
-  while( millis() < 5000 ) {
-
-    float val0 = analogRead( A0 );
-    float val1 = analogRead( A1 );
-    
-    min0 = min( min0, val0 );
-    max0 = max( max0, val0 );
-    min1 = min( min1, val1 );
-    max1 = max( max1, val1 );
-    
-    delay( 50 );
-  }
+  elbow.attach( 2 );
+  wrist.attach( 3 );
 }
 
 void loop() {
+
+  float volt0 = 0, volt1 = 0;
+  for(int n = 0; n < 10; n++) {
+    volt0 += analogRead( A0 ) / 10.0;
+    volt1 += analogRead( A1 ) / 10.0;
+    delay(5);
+  }
+
+  min0 = min( min0, volt0 );
+  max0 = max( max0, volt0 );
+  min1 = min( min1, volt1 );
+  max1 = max( max1, volt1 );
   
   // Joint angle mapping
-  int angElbow = map( analogRead( A0 ), min0, max0, 0, 150 );
-  int angWrist = map( analogRead( A1 ), min1, max1, 0, 170 );
+  int angElbow = map( volt0, min0, max0, 10, 150 );
+  int angWrist = map( volt1, min1, max1, 10, 170 );
   
   // Actuate
   elbow.write( angElbow );
   wrist.write( angWrist );
-  
-  delay(50);
 }
